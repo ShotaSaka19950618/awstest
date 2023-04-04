5f7e1288fa36358228a40bd16c8b6a46b0e23570
Shota.S <geminis218@icloud.com>
2023-04-04 17:51:13 +0900

    test

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 const express = require("express");
 const cors = require("cors");
-const app = express();
-const userRouter = require("./routes/users");
-const postRouter = require("./routes/posts");
-const notificationRouter = require("./routes/notifications");
-const authRouter = require("./routes/auth");
-const uploadRouter = require("./routes/upload");
 const mongoose = require("mongoose");
 const helmet = require("helmet");
+const app = express();
+// const userRouter = require("./routes/users");
+// const postRouter = require("./routes/posts");
+// const notificationRouter = require("./routes/notifications");
+// const authRouter = require("./routes/auth");
+// const uploadRouter = require("./routes/upload");
+
+// CORS対策
+app.use(cors());
+
+// 脆弱性対策
+app.use(helmet());
 
 
 
