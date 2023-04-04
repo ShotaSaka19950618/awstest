180eb4fa0dfdbf840984103cd143e0688a6a097d
Shota.S <geminis218@icloud.com>
2023-04-04 22:59:57 +0900

    test

--- routes/upload.js
+++ routes/upload.js
@@ -1,10 +1,11 @@
 const router = require("express").Router();
 const isAuth = require("../middleware/isAuth");
-const fileUpload = require("../middleware/fileUpload");
+// const fileUpload = require("../middleware/fileUpload");
 
 // 画像upload
-router.post("/", isAuth, fileUpload.single("file"), (req, res) => {
-  try {
+// router.post("/", isAuth, fileUpload.single("file"), (req, res) => {
+router.post("/", isAuth, (req, res) => {
+    try {
     return res.json({
       success: true,
       message: "画像をアップロードしました！",
